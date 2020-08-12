b5a680c33c504bf1bb7dfbbf7ec15dd38d87e8b7
Romit TAJALE <tajale72@gmail.com>
2020-08-11 22:28:52 -0500

    first Api

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", Welcome)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome, we have what you need!!")
+	fmt.Fprintf(w, "Coffe for everyone")
+}
