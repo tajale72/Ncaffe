64a3dc5856f1e8e98b3154cbb4c65b0d2c5c3c92
Romit TAJALE <tajale72@gmail.com>
2020-08-12 13:19:33 -0500

    Sample Page

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 func main() {
 
 	mux := http.NewServeMux()
+	// creating a object for handler func
 
 	mux.HandleFunc("/", Welcome)
+	//Creating a handler function and Welcome is an endpoint
 
 	log.Println("Starting server on :4000")
+	//Print statement
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
