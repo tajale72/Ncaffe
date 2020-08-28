6ab11b5066d39f6823eae9eddfce64b9c39f4f57
tajale72 <tajale72@gmail.com>
2020-08-27 23:19:53 -0500

    angular file

---
+++ hello-angular4/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { DateComponent } from './date/date.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HerosComponent,
+    DateComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+}
