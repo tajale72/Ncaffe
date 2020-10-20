4926feaa77c40cba729289de4073e673ff630b77
Tajale72 <Tajale72@gmail.com>
2020-10-20 16:13:32 -0500

    user file updates

--- registration-service/src/main/java/com/javatechie/reg/service/api/model/User.java
+++ registration-service/src/main/java/com/javatechie/reg/service/api/model/User.java
@@ -25,47 +25,47 @@ public class User {
     private int experience;
     private String domain;
     
-//    public User() {
-//    	
-//    }
-//	public User(int id, String name, String email, int experience, String domain) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//		this.email = email;
-//		this.experience = experience;
-//		this.domain = domain;
-//	}
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getEmail() {
-//		return email;
-//	}
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//	public int getExperience() {
-//		return experience;
-//	}
-//	public void setExperience(int experience) {
-//		this.experience = experience;
-//	}
-//	public String getDomain() {
-//		return domain;
-//	}
-//	public void setDomain(String domain) {
-//		this.domain = domain;
-//	}
-//    
-//    
+    public User() {
+    	
+    }
+	public User(int id, String name, String email, int experience, String domain) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.experience = experience;
+		this.domain = domain;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getExperience() {
+		return experience;
+	}
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+    
+    
 }
