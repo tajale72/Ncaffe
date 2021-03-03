ca674ba788af282da082bfd4a8529fcc779ca750
Tajale72 <Tajale72@gmail.com>
2021-03-03 12:42:21 -0600

    initial commit

--- hello.go
+++ hello.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("hello \")
+	line := "hello\n"
+	line = strings.TrimSuffix(line, "\n") // remove newline
+	fmt.Printf("'%s'\n", line)
 
+	// line = strings.TrimSuffix(line, "\n") // not bothered if there was no newline
+	// fmt.Printf("'%s'\n", line)
+
+	// line = "hello\n\n"
+	// line = strings.TrimSuffix(line, "\n") // removing only one newline
+	// fmt.Printf("'%s'\n", line)
 }
